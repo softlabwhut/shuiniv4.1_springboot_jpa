87edbe0da52b27a6b676c0beb70438926428099a
danielwu927 <danielwu927@outlook.com>
2018-06-13 01:21:55 +0800

    测试

---
+++ src/main/java/com/whut/springbootjpacementv4_1/service/Test.java
@@ -0,0 +1,16 @@
+package com.whut.springbootjpacementv4_1.service;
+
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class Test {
+    public static void main(String[] args) {
+        String pas="dadadadad";
+        BCryptPasswordEncoder bCryptPasswordEncoder=new BCryptPasswordEncoder();
+        String pasencode=bCryptPasswordEncoder.encode(pas);
+        String pasencode2=bCryptPasswordEncoder.encode(pas);
+        System.out.println(pasencode);
+        System.out.println(pasencode2);
+
+    }
+}
