e5e062ec34e7c3a18219e0058753231e2dede0c7
danielwu927 <danielwu927@outlook.com>
2018-07-03 00:40:40 +0800

    增加调查表实体类

--- src/main/java/com/whut/springbootjpacementv4_1/entity/QuestionnaireBasic.java
+++ src/main/java/com/whut/springbootjpacementv4_1/entity/QuestionnaireBasic.java
@@ -1,6 +1,7 @@
 package com.whut.springbootjpacementv4_1.entity;
 
 import javax.persistence.*;
+import java.sql.Date;
 
 @Entity
 @Table(name = "questionnaire_basic")
@@ -11,13 +12,58 @@ public class QuestionnaireBasic
     @Column(name = "quest_id")
     private Integer id;
 
+    @Column(name = "comp_name")
     private String compName;
 
+    @Column(name = "designer_name")
     private String designerName;
 
+    @Column(name = "day_capacity")
     private Integer dayCapacity;
 
+    @Column(name = "kin_size")
     private String kinSize;
+
+    @Column(name = "pre_heater")
+    private String preHeater;
+
+    @Column(name = "dslgx_power")
+    private Integer dslgxPower;
+
+    @Column(name = "burning_heat")
+    private Integer burningHeat;
+
+    @Column(name = "operate_rate")
+    private Double operateRate;
+
+    @Column(name = "waste_gen_electr")
+    private Double wasteGenElectr;
+
+    @Column(name = "region_id")
+    private Byte regionId;
+
+    @Column(name = "submit_date")
+    private Date submitDate;
+
+    @Column(name = "submiter")
+    private String submiter;
+
+    @Column(name = "line_number")
+    private Integer lineNumber;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
