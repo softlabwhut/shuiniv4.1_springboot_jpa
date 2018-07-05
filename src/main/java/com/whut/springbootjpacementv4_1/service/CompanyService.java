762b8d86c85d98626ca1b25f71de26ebf8bab5bd
danielwu927 <danielwu927@outlook.com>
2018-07-05 15:17:20 +0800

    优化gitignore设置

--- src/main/java/com/whut/springbootjpacementv4_1/service/CompanyService.java
+++ src/main/java/com/whut/springbootjpacementv4_1/service/CompanyService.java
@@ -9,12 +9,6 @@ import java.util.List;
 public interface CompanyService
 {
     List<Company> getFirstFiftyCompanies();
-//    Result addCompany(Integer id, Integer region_id,
-//                      String name, String address,
-//                      String postcode, Integer capacity,
-//                      String status, String phone_number,
-//                      Integer year_capacity, String remark,
-//                      String invited_person, String wj_title,
-//                      Date start_date, Date end_date);
+      Result addCompany(Company company);
 
 }
